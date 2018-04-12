237478689da1d6588fb81871953644c8be00e845
girijabehera <girija.behera@gmail.com>
2018-04-13 00:21:45 +0530

    Initial commit

---
+++ BBGChatNLPDemo/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace BBGChatNLPDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblProcessedData = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtChat = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Location = new System.Drawing.Point(716, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(585, 588);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblProcessedData
+            // 
+            this.lblProcessedData.AutoEllipsis = true;
+            this.lblProcessedData.AutoSize = true;
+            this.lblProcessedData.Location = new System.Drawing.Point(19, 24);
+            this.lblProcessedData.Name = "lblProcessedData";
+            this.lblProcessedData.Size = new System.Drawing.Size(12, 17);
+            this.lblProcessedData.TabIndex = 1;
+            this.lblProcessedData.Text = " ";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.lblProcessedData);
+            this.panel1.Location = new System.Drawing.Point(8, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(568, 566);
+            this.panel1.TabIndex = 2;
+            // 
+            // txtChat
+            // 
+            this.txtChat.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtChat.Location = new System.Drawing.Point(32, 36);
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(664, 398);
+            this.txtChat.TabIndex = 2;
+            this.txtChat.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(332, 480);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(107, 36);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Process";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(460, 480);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(107, 36);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(588, 480);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(107, 36);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1327, 638);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblProcessedData;
+        private System.Windows.Forms.RichTextBox txtChat;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ BBGChatNLPDemo/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BBGChatNLPDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/275836/multiple-colors-in-a-c-sharp-net-label
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="output"></param>
+        /// <returns></returns>
+        private string format_text(string input, string output)
+        {
+            string parsedData = string.Empty;
+            parsedData = "INPUT: " + input + System.Environment.NewLine + System.Environment.NewLine;
+            parsedData = parsedData + "ZIPLINE DATA: " + output;
+            parsedData = parsedData + System.Environment.NewLine + System.Environment.NewLine + "-----------------------------------------------------------------";
+            return parsedData;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //string result = run_cmd("@C:/Python26/python.exe", "C://Python26//test.py 100");
+            string parsed = format_text(txtChat.Text, "parsed data");
+            lblProcessedData.Text = lblProcessedData.Text + System.Environment.NewLine + parsed;
+
+        }
+
+        private string run_cmd(string cmd, string args)
+        {
+            string result = "parsed Text";
+            //ProcessStartInfo start = new ProcessStartInfo();
+            //start.FileName = cmd;//cmd is full path to python.exe | @C:/Python26/python.exe
+            //start.Arguments = args;//args is path to .py file and any cmd line args | C://Python26//test.py 100
+            //start.UseShellExecute = false;
+            //start.RedirectStandardOutput = true;
+            //using (Process process = Process.Start(start))
+            //{
+            //    using (StreamReader reader = process.StandardOutput)
+            //    {
+            //        result = reader.ReadToEnd();
+            //    }
+            //}
+
+            return result;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblProcessedData.ResetText();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
